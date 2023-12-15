958349321baddbf7f707d0b66c953f48e68a4cc1
Alex-Haidai <aleksgayda@gmail.com>
2023-12-15 14:59:19 +0200

    Added DTO classes

---
+++ CCL/Security/SecurityContext.cs
@@ -0,0 +1,24 @@
+﻿using CCL.Security.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCL.Security
+{
+    public static class SecurityContext
+    {
+        static Account _account = null;
+
+        public static Account GetAccount()
+        {
+            return _account;
+        }
+
+        public static void SetAccount(Account account)
+        {
+            _account = account;
+        }
+    }
+}
